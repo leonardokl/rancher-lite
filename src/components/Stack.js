60fd9af7cbcc02b0d06419390165553817cc4c98
Leonardo Luiz <leonardokl@hotmail.com>
2018-11-19 13:34:30 -0300

    add key events to stack

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -4,8 +4,13 @@ const Stack = ({ data, onClick }) => (
   <div className="stack-section" style={{ overflow: "hidden" }}>
     <div
       className="clearfix well r-p0 r-m0"
+      tabIndex={0}
       style={{ cursor: "pointer" }}
+      role="button"
       onClick={onClick}
+      onKeyPress={({ key }) => {
+        if (key === "Enter") onClick();
+      }}
     >
       <div className="header-left " style={{ marginLeft: 10, float: "none" }}>
         <h4 className="divider clip stack-link" title={data.name}>
